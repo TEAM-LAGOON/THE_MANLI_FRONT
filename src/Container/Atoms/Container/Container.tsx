1e8061d87659d288141551458dc7ee4c0913bbcc
yongbeomKim1 <dltmfrl600@gmail.com>
2022-11-25 17:47:54 +0900

    feat: container component

---
+++ src/Container/Atoms/Container/Container.tsx
@@ -0,0 +1,16 @@
+import { ContainerPropsType } from './Container.types';
+import styled from '@emotion/styled';
+
+//todo: header 및 필요한 추가 속성
+const Container: React.FC<ContainerPropsType> = ({ className = '', children }) => {
+  return <Root className={className}>{children}</Root>;
+};
+export default Container;
+
+const Root = styled.div`
+  font-size: 2rem;
+
+  ${props => props.theme.mq.mobile} {
+    margin: 0 2rem;
+  }
+`;
