35421f0146e5745f0932636d4258e549efa73316
yongbeomKim1 <dltmfrl600@gmail.com>
2022-11-25 18:19:14 +0900

    fix: letter-spacing

--- src/styles/reset.ts
+++ src/styles/reset.ts
@@ -2,11 +2,10 @@ import { css } from '@emotion/react';
 
 export const reset = css`
   * {
-    box-sizing: border-box;
     margin: 0;
     padding: 0;
-    letter-spacing: -0.02em;
   }
+
   ul,
   li {
     list-style: none;
