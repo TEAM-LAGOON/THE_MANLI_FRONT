d1f6cdc7e11d57d4d78351c7137b53d6301bb0cc
bmlee531@gmail.com <bmlee531@gmail.com>
2022-11-27 16:04:03 +0900

    feat: button

---
+++ src/components/Atoms/Button/Button.types.ts
@@ -0,0 +1,13 @@
+export interface ButtonPropsType {
+  children?: React.ReactNode;
+  onAction?: () => void;
+  className?: string;
+  padding?: string;
+  bg?: string;
+  color?: string;
+  fontSize?: string;
+  border?: string;
+  radius?: string;
+  hoverBg?: string;
+  hoverColor?: string;
+}
