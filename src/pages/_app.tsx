fa0a0777fef6a0e838716fb2f839d718e617e00b
bmlee531@gmail.com <bmlee531@gmail.com>
2022-11-27 16:05:13 +0900

    chore: code conventions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from '@emotion/react';
 import { Global } from '@emotion/react';
 import { global } from '../styles/global';
-import { ThemeProvider } from '@emotion/react';
 import theme from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
