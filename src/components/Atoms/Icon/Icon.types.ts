a3bc33f83d192a26efe171a61fca4d083a53fd7c
yongbeomKim1 <dltmfrl600@gmail.com>
2022-11-27 18:10:30 +0900

    feat: Icon component

---
+++ src/components/Atoms/Icon/Icon.types.ts
@@ -0,0 +1,9 @@
+import * as svg from '../../../../public/svgs';
+
+export type IconType = keyof typeof svg;
+
+export interface IconPropsType {
+  name: IconType;
+  className?: string;
+  style?: React.CSSProperties;
+}
