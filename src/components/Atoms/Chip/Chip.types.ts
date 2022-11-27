2fb43fa3fcde89db5e74be152d33fe40926fd55a
yongbeomKim1 <dltmfrl600@gmail.com>
2022-11-27 18:11:38 +0900

    feat: Chip component

---
+++ src/components/Atoms/Chip/Chip.types.ts
@@ -0,0 +1,8 @@
+export interface ChipPropsType {
+  className?: string;
+  padding?: string;
+  bgColor?: string;
+  color?: string;
+  borderRadius?: string;
+  children?: React.ReactNode | string;
+}
