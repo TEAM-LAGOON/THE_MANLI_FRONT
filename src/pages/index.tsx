7f4111a6577a76366b32b632f42c52b51f96a28d
yongbeomKim1 <dltmfrl600@gmail.com>
2022-12-02 23:07:30 +0900

    chore: 폴더 구조 변경으로 import 구문 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
 import type { NextPage } from 'next';
-import Container from '../components/Atoms/Container/Atoms/Container';
+import { Container, Chip, Avatar, Icon, Text } from '../components';
 
+const props = {
+  children: '만리',
+};
 const Home: NextPage = () => {
   return (
     <Container>
       <div>dd</div>
+      <Chip {...props} />
+      <Avatar imgSrc="/images/avatar.png" />
+      <Icon name={'fire'}></Icon>
+      <Text
+        type={'m-semibold-5'}
+        value={`화이팅\n해보자구`}
+        align={'left'}
+        color={'var(--primary-400)'}
+      ></Text>
     </Container>
   );
 };
