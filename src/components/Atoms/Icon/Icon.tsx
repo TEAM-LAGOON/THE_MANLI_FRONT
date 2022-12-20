3743498c430cd0e3af3f0ea106c0ad1a560a11e7
yongbeomKim1 <dltmfrl600@gmail.com>
2022-12-20 16:10:18 +0900

    feat: Icon onClick event

--- src/components/Atoms/Icon/Icon.tsx
+++ src/components/Atoms/Icon/Icon.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { IconPropsType } from './Icon.types';
 import * as svg from '../../../../public/svgs';
 
-export default function Icon({ name, className, style }: IconPropsType) {
+export default function Icon({ name, className, style, onClick }: IconPropsType) {
   return React.createElement(svg[name], {
     className,
+    onClick,
     style,
   });
 }
