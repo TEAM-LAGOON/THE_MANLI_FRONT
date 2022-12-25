9bad93a1d43e7b069a8bda5faa2a4cc37cc62430
yongbeomKim1 <dltmfrl600@gmail.com>
2022-12-26 04:00:36 +0900

    feat: input props

--- src/components/Atoms/Input/Input.tsx
+++ src/components/Atoms/Input/Input.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 import { InputPropsType } from './Input.types';
 
 const Input: React.FC<InputPropsType> = ({ ...props }) => {
-  const { inputValue, placeholderText, onAction } = props;
+  const { inputValue, placeholderText, onClick, onAction, inputName } = props;
 
   return (
     <Container
@@ -10,7 +10,9 @@ const Input: React.FC<InputPropsType> = ({ ...props }) => {
       {...props}
       placeholder={placeholderText}
       onChange={onAction}
+      onClick={onClick}
       value={inputValue}
+      name={inputName}
     />
   );
 };
@@ -25,5 +27,7 @@ const Container = styled.input<InputPropsType>`
   font-size: ${({ fontSize }) => (fontSize ? fontSize : '1rem')};
   border: ${({ border }) => (border ? border : '1px solid #767676')};
   border-radius: ${({ radius }) => (radius ? radius : '12.375rem')};
+  box-sizing: border-box;
+  line-height: 150%;
   text-overflow: ellipsis;
 `;
