d65180adc430478418d28bc7b761916bde6d599e
yongbeomKim1 <dltmfrl600@gmail.com>
2022-12-27 06:04:24 +0900

    update: font

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -7,4 +7,11 @@ export const font = css`
     src: url('/fonts/Pretendard-Regular.woff') format('woff');
     font-weight: 400;
   }
+
+  @font-face {
+    font-family: 'ONE-Mobile-Title';
+    src: url('/fonts/ONE-Mobile-Title.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
 `;
