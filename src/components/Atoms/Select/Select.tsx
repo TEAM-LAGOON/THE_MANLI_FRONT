d01f121a9341c749a0aa7b456405c759ae15ef95
bmlee531@gmail.com <bmlee531@gmail.com>
2022-12-26 15:10:57 +0900

    update: select components

--- src/components/Atoms/Select/Select.tsx
+++ src/components/Atoms/Select/Select.tsx
@@ -1,9 +1,9 @@
 import styled from '@emotion/styled';
-import { SetStateAction } from 'react';
+import Icon from '../Icon';
 import { SelectPropsType } from './Select.types';
 
 const Select: React.FC<SelectPropsType> = ({ ...props }) => {
-  const { selectList, selectTitle, setSelectedValue } = props;
+  const { selectList, selectTitle, setSelectedValue, iconName } = props;
 
   const selectHandleChange = (e: { target: { value: string } }) => {
     const {
@@ -14,25 +14,51 @@ const Select: React.FC<SelectPropsType> = ({ ...props }) => {
   };
 
   return (
-    <SelectContainer name="select" onChange={selectHandleChange}>
-      {selectTitle && (
-        <option selected disabled>
-          {selectTitle}
-        </option>
-      )}
-      {selectList?.map((text, index) => (
-        <option value={text} key={index}>
-          {text}
-        </option>
-      ))}
-    </SelectContainer>
+    <Container>
+      <SelectContainer name="select" onChange={selectHandleChange}>
+        {selectTitle && (
+          <option selected disabled>
+            {selectTitle}
+          </option>
+        )}
+        {selectList?.map((text, index) => (
+          <option value={text} key={index}>
+            {text}
+          </option>
+        ))}
+      </SelectContainer>
+      <Icon name={iconName} className={iconName} />
+    </Container>
   );
 };
 
 export default Select;
+const Container = styled.div`
+  position: relative;
 
+  svg {
+    position: absolute;
+    left: 9rem;
+    background-color: #fff;
+    width: 3rem;
+    stroke: var(--secondary-700);
+  }
+  & .swap {
+    top: 0.8rem;
+  }
+  & .caret_down_outline {
+    top: 1.2rem;
+  }
+`;
 const SelectContainer = styled.select`
+  appearance: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+
+  width: 12.125rem;
   border: none;
-  color: #597bad;
+  color: var(--secondary-700);
   padding: 0.75rem;
+  font-size: 1.75rem;
+  font-weight: 600;
 `;
--- src/components/Atoms/Select/Select.types.ts
+++ src/components/Atoms/Select/Select.types.ts
@@ -2,4 +2,5 @@ export interface SelectPropsType {
   selectList: string[];
   selectTitle?: string;
   setSelectedValue: Function;
+  iconName: 'caret_down_outline' | 'swap';
 }
