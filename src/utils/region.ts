fd326d3bb01adade7f9270defbf08c4f07b3af72
yongbeomKim1 <dltmfrl600@gmail.com>
2023-01-30 19:00:14 +0900

    feat: region option

---
+++ src/utils/region.ts
@@ -0,0 +1,46 @@
+export const RegionList = [
+  {
+    id: 1,
+    name: '서울',
+  },
+  {
+    id: 2,
+    name: '경기도',
+  },
+  {
+    id: 3,
+    name: '인천',
+  },
+  {
+    id: 4,
+    name: '대구',
+  },
+  {
+    id: 5,
+    name: '부산',
+  },
+  {
+    id: 6,
+    name: '제주',
+  },
+  {
+    id: 7,
+    name: '대전',
+  },
+  {
+    id: 8,
+    name: '광주',
+  },
+  {
+    id: 9,
+    name: '경상도',
+  },
+  {
+    id: 10,
+    name: '전라도',
+  },
+  {
+    id: 11,
+    name: '충청도',
+  },
+];
